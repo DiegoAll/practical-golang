5f2af22054a03238e742f9e48f101d1be700bbbc
DiegoAll <dposadallano@gmail.com>
2021-09-28 22:23:13 -0500

    functions and variables visibility and defer keyword

--- src/main.go
+++ src/main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import "fmt" //Area donde se importan las librerias encesrias para ejecutar el programa
+
+func privada() {
+	fmt.Println("Ejecutar alguna lógica que no requiere ser exportada")
+}
+
+func Publica() {
+	fmt.Println("Ejecutar alguna lógica que pretenfo exportar")
+}
+
+func saludar() {
+	defer fmt.Println("Buenas tardes") //Esta porción de código debe ser ejecutado hasta que la función haya terminado.
+	fmt.Println("¿Cómo se encuentra?")
+}
 
 func main() {
 
-	fmt.Print("Que mas pues parcero")
+	var message string = "Que se dice parcero"
+	easyMessage := "Que se dice parcero usando :="
+	fmt.Println(message)
+	fmt.Println(easyMessage)
+
+	//Comentarios !!!
+	a := 10.
+	var b float64 = 3
+	fmt.Println((a / b))
+
+	var c int = 10
+	d := 3
+	fmt.Println(c / d)
+	x := true
+	y := false
+	fmt.Println((x || y))
+	fmt.Println(y && x)
+	fmt.Println(!x)
+	privada()
+	Publica()
+	saludar()
+
 }
